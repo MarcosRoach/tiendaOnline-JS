bc2ba3fa253df9c3dad5d381c53a860f6b3c19bc
MarcosRoach <roachmarcos@hotmail.com>
2023-02-17 12:03:05 -0300

    c error 3

---
+++ js/fetchCatalogo2.js
@@ -0,0 +1,31 @@
+let catalogo = [];
+// consumir JSON de Catalogo
+
+async function cargarCatalogo() {
+  const response = await fetch("../productos.json");
+  // extaer datos del JSON en forma de objetos
+  const data = await response.json();
+
+  // le asigno clase a cada objeto
+  for (let producto of data) {
+    nuevoProducto = new Producto(
+      producto.id,
+      producto.nombre,
+      producto.descripcion,
+      producto.rubro,
+      producto.marca,
+      producto.precio,
+      producto.oferta,
+      producto.descuentoPorcentaje,
+      producto.precioDescuento,
+      producto.imagen,
+      producto.imagen_sm
+    );
+
+    // agrego el producto con clase al array
+    catalogo.push(nuevoProducto);
+  }
+}
+
+// Cargar el catalogo
+cargarCatalogo();+
